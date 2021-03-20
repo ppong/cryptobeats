d239f46823b48c3e711190bacfad67cf0dc509d3
Marcel O'Neil <marcel@marceloneil.com>
2021-03-20 16:44:59 -0400

    song player

--- webapp/src/components/footer/index.tsx
+++ webapp/src/components/footer/index.tsx
@@ -1,24 +1,41 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
 
 import { useAppContext } from '../context/application';
 
+// from https://stackoverflow.com/a/47686478
+const useAudio = (url: string): [boolean, () => void] => {
+  const [audio] = useState(new Audio(url));
+  const [isPlaying, setPlaying] = useState<boolean>(false);
+
+  const togglePlayback = () => setPlaying(!isPlaying);
+
+  useEffect(() => {
+    isPlaying ? audio.play() : audio.pause();
+  }, [audio, isPlaying]);
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    }
+  })
+
+  return [isPlaying, togglePlayback];
+}
+
 export function Footer() {
   const { title, artist } = useAppContext();
-  const [isActive, setIsActive] = useState<boolean>(false);
-
-  const togglePlayback = () => {
-    setIsActive(!isActive);
-  }
+  const [isPlaying, togglePlayback] = useAudio('https://ipfs.io/ipfs/bafybeidkinok24hq4j3aoqbpcwl4befibdlysvd764frmzhlwbxfshqsaa');
 
   return (
     <div className='absolute bottom-0 w-full'>
       <div className='flex justify-center items-center'>
         <div className='my-16 flex items-center'>
           <div onClick={togglePlayback} className='flex justify-center items-center rounded-full w-16 h-16 border-2 border-gray-50'>
-            <FontAwesomeIcon className='text-gray-50' icon={isActive ? faPause : faPlay} size='1x' />
+            <FontAwesomeIcon className='text-gray-50' icon={isPlaying ? faPause : faPlay} size='1x' />
           </div>
           <div className='ml-4'>
             <div className='text-gray-300'>
